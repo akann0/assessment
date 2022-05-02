18d474a3f7f8ee50cfa3baa90587288c01bfe43b
akann0 <akann@ucsd.edu>
2022-05-01 21:54:02 -0700

    Assessment Create

---
+++ demofile.java
@@ -0,0 +1,8 @@
+class demofile {
+    static String seven(){
+        return Integer.toString(6);
+    }
+    public static void main(String[] args) {
+        System.out.println(seven());
+    }
+}
---
+++ demotest.java
@@ -0,0 +1,10 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class demotest {
+    @Test 
+    public void test(){
+        assertEquals("7", demofile.seven());
+    }
+
+}
